62261954aa0353fc944b9ab1567d60cedc95a210
Kallal Mukherjee <ritamukherje62@gmail.com>
2025-03-03 23:43:41 +0530

    Create main2.js

---
+++ main2.js
@@ -0,0 +1,19 @@
+// let date = new Date();
+// console.log(date.getTime());
+// console.log(date.getFullYear());
+// console.log(date.getDay());
+
+// let car ={
+// name:"kajejnnec",
+// year:23133,
+
+// start:function() {
+//     console.log("my car")
+// }
+// };
+// console.log(car)
+
+// let age = 18;
+// let isAdult = age >= 18 ? "yes" : "no";
+// console.log(isAdult);   
+if (1) { console.log("This will run."); }   
